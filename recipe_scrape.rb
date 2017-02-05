51a9d6712c56f1f8ed3d1d884f62035db03b97d6
jsteenb2 <jonathansteenbergen@gmail.com>
2017-02-04 21:17:46 -0600

    Updated scraper to traverse entire site

--- recipe_scrape.rb
+++ recipe_scrape.rb
@@ -1,55 +1,140 @@
-require 'nokogiri'
+require 'mechanize'
 require 'open-uri'
+require 'pry-byebug'
 
-recipes = {
-    "Chicken Vindaloo": 'http://www.rachnas-kitchen.com/chicken-vindaloo/',
-    "Tamarind Chutney": 'http://www.rachnas-kitchen.com/sweet-tamarind-chutney-saunth-chutney-tamarind-sauce/',
-    "Mint Chutney": 'http://www.rachnas-kitchen.com/green-chutney-recipe/',
-    "Hyderabadi Chicken Biryani": 'http://www.rachnas-kitchen.com/hyderabadi-chicken-biryani-recipe/',
-    "Chicken Korma": 'http://www.rachnas-kitchen.com/chicken-korma-recipe/',
-    "Kadai Chicken": "http://www.rachnas-kitchen.com/kadai-chicken-recipe-chicken-karahi/",
-    "Butter Chicken": 'http://www.rachnas-kitchen.com/indian-butter-chicken-curry/',
-    "Instant Whole Wheat Naan": 'http://www.rachnas-kitchen.com/whole-wheat-naan-naan-recipe-without-yeast/',
-    "Butter Naan": "http://www.rachnas-kitchen.com/naan-bread-recipe/",
-    "Makki Ki Roti": 'http://www.rachnas-kitchen.com/makki-ki-roti-how-to-make-makki-di-roti/',
-    "Mango Lassi": 'http://www.rachnas-kitchen.com/mango-lassi-recipe/',
-    "Masoor Dal": 'http://www.rachnas-kitchen.com/masoor-dal-recipe/',
-    "Dal Fry": 'http://www.rachnas-kitchen.com/dal-fry-recipe/',
-    "Dal Makhani": 'http://www.rachnas-kitchen.com/traditional-dal-makhani-recipe/',
-    "Kadia Palak": 'http://www.rachnas-kitchen.com/kadai-paneer-recipe-restaurant-style/',
-    "Palak Paneer": 'http://www.rachnas-kitchen.com/palak-paneer-recipe/',
-    "Homemade Ghee": 'http://www.rachnas-kitchen.com/how-to-make-ghee/'
-}
+class RecipeScraper
+    attr_reader :pages
+    def initialize
+        @agent = Mechanize.new
+        @pages = {}
+        @page = @agent.get('http://www.rachnas-kitchen.com')
+    end
 
-recipes.each do |key, value|
-    file_name = key.to_s.downcase.split(" ").join("_") + ".txt"
-    next if File.exists? file_name
+    def grab_categories
+        find_links(@page, "#menu-secondary-menu li", @pages)
+    end
 
-    doc = Nokogiri::HTML(open(value)) 
-
-    ingredients = doc.css('.ingredient')
-    instructions = doc.css '.instruction'
-    
-
-    recipe_title = "#{ key.to_s } courtesy #{ value }" 
-
-    File.open(file_name, 'w+') do |file|
-        file.puts recipe_title
-        file.puts
-
-        file.puts "INGREDIENTS"
-        file.puts "------------"
-        ingredients.each do |ingredient|
-            file.puts ingredient.text
-        end
-        
-        file.puts
-        file.puts "INSTRUCTIONS"
-        file.puts "------------"
-
-        instructions.each_with_index do |instruction, index|
-            instr_line = "#{index + 1}) #{instruction.text}"
-            file.puts instr_line
+    def grab_recipes
+        @pages.each do |title, node_obj|
+            category_page = node_obj[:node].click
+            recipe_links = find_links(category_page, "#genesis-content h2 a")
+            node_obj[:recipes] = recipe_links
         end
     end
+
+    def build_recipes
+        @pages.each do |title, node_obj|
+            node_obj[:recipes].each do |recipe_node|
+                write_recipe(recipe_node, node_obj)
+                binding.pry
+            end
+        end
+    end
+
+    private 
+
+        def format_page_info(link_node, category_node)
+            title = link_node[:node].text.split("|")[0].strip
+            file_name = title.to_s.downcase.split(" ").join("_") + ".txt"
+            recipe_header = "#{ title } courtesy #{ url }"
+            {
+                page_url: link_node[:node].href,
+                page_title: title,
+                file_name: file_name,
+                recipe_header: recipe_header
+            }
+        end
+
+        def write_recipe(link_node, category_node)
+            file_info = format_page_info(link_node, category_node)
+            # url = link_node[:node].href
+            # title = link_node[:node].text.split("|")[0].strip
+            # file_name = title.to_s.downcase.split(" ").join("_") + ".txt"
+            # next if File.exists? file_name
+
+            doc = Nokogiri::HTML(open(file_info[:url])) 
+
+            ingredients = doc.css '.ingredient'
+            instructions = doc.css '.instruction'
+            
+
+            # recipe_title = "#{ title } courtesy #{ url }" 
+
+            File.open(file_name, 'w+') do |file|
+                file.puts file_info[:recipe_header]
+                file.puts
+
+                file.puts "INGREDIENTS"
+                file.puts "------------"
+                ingredients.each do |ingredient|
+                    file.puts ingredient.text
+                end
+                
+                file.puts
+                file.puts "INSTRUCTIONS"
+                file.puts "------------"
+
+                instructions.each_with_index do |instruction, index|
+                    instr_line = "#{index + 1}) #{instruction.text}"
+                    file.puts instr_line
+                end
+            end
+        end
+
+        def find_links(page, selector, container = {})
+            link_texts = find_link_text(page, selector)
+            link_texts.map do |link_text|
+                container[link_text] = {
+                    node: page.links.find { |link| link_text == link.text }
+                }
+            end
+        end
+
+        def find_link_text(page, selector)
+            page.css(selector)
+                .map { |link| link.text }
+                .reject { |link_text| (link_text == "\n") || 
+                                    (link_text.downcase.include? "video") 
+                        }
+        end
 end
+
+go_get_em = RecipeScraper.new
+go_get_em.grab_categories
+go_get_em.grab_recipes
+go_get_em.build_recipes
+pp go_get_em.pages
+
+
+# recipes.each do |key, value|
+    # file_name = key.to_s.downcase.split(" ").join("_") + ".txt"
+    # next if File.exists? file_name
+
+    # doc = Nokogiri::HTML(open(value)) 
+
+    # ingredients = doc.css('.ingredient')
+    # instructions = doc.css '.instruction'
+    
+
+    # recipe_title = "#{ key.to_s } courtesy #{ value }" 
+
+    # File.open(file_name, 'w+') do |file|
+    #     file.puts recipe_title
+    #     file.puts
+
+    #     file.puts "INGREDIENTS"
+    #     file.puts "------------"
+    #     ingredients.each do |ingredient|
+    #         file.puts ingredient.text
+    #     end
+        
+    #     file.puts
+    #     file.puts "INSTRUCTIONS"
+    #     file.puts "------------"
+
+    #     instructions.each_with_index do |instruction, index|
+    #         instr_line = "#{index + 1}) #{instruction.text}"
+    #         file.puts instr_line
+    #     end
+    # end
+# end
